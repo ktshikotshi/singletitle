8336cd619528b0d2be01b254e9a6ff5645348b3e
Khuthadzo Tshikotshi <khuthadzotshikotshi@gmail.com>
2017-10-18 13:32:17 +0200

    changes to file names, fix view products table

--- calculator/client/viewProducts.php
+++ calculator/client/viewProducts.php
@@ -1,21 +1,29 @@
+<?php
+	include("../../header.php");
+    include("../scripts/_getAllProductsFormatted.php");
+?>
+<html>
+<head>
+<?php getStyles(); ?>
 <style type="text/css">
 #apDiv1 {
 	position:absolute;
 	left:12px;
-	top:90px;
+	/*top:90px;*/
 	width:593px;
 	height:68px;
-	z-index:1;
 }
 #apDiv2 {
 	position:absolute;
 	left:611px;
-	top:91px;
+	/*top:91px;*/
 	width:247px;
 	height:120px;
-	z-index:2;
 }
 </style>
+</head>
+<body>
+<?php getHeader(); ?>
 <div id="apDiv1">
   <table width="578" border="0">
     <tr>
@@ -25,50 +33,16 @@
       <td width="127">Price</td>
       <td width="66">&nbsp;</td>
     </tr>
-    <tr>
-      <td><?php
-    	include("../scripts/_getAllProductsFormatted.php");
+	<?php
 		while($row = mysql_fetch_array($result)){
-			echo '<a href="../domain/viewProductPage.php?ID='.$row['price'].'"><img src="../buttons/button_home.jpg" width="71" height="17" /></a>';
-			echo "<br/>";
+			echo '<tr><td><a href="../domain/viewProductPage.php?ID='.$row['price'].'"><img src="../buttons/button_home.jpg" width="71" height="17" /></a></td>';
+			echo "<td>".$row['product_name']."</td>";
+			echo "<td>".$row['category_name']."</td>";
+			echo '<td><a href="viewCalculator.php?ID='.$row['product_id'].'"><img src="../buttons/button_view.jpg" width="71" height="17" /></a>';
+
+			echo "</td></tr>";
 		}
 	?>
-        &nbsp;</td>
-      <td><?php
-    	$con = mysql_connect($localhost, $database_user, $database_password);	
-			if (!$con){
-				die('Something definitely went wrong.. You might want to look this up: '.mysql_error());
-			}
-		mysql_select_db($database_name, $con);
-		$result = mysql_query("SELECT singletitle_product_list.product_id, singletitle_product_list.product_name, singletitle_product_category_list.category_name,
-								singletitle_product_category_list.category_price, singletitle_price_list.price 
-								FROM singletitle_price_list JOIN singletitle_product_category_list JOIN singletitle_product_list
-								WHERE singletitle_product_list.product_id = singletitle_price_list.product_id AND singletitle_product_list.category_id = 	             
-								singletitle_product_category_list.category_id AND singletitle_product_list.product_status = '1'");
-		while($row = mysql_fetch_array($result)){
-			echo $row['product_name'];
-			echo "<br/>";
-		}
-	?>
-        &nbsp;</td>
-      <td><?php
-    	include("../scripts/_getAllProductsFormatted.php");
-		while($row = mysql_fetch_array($result)){
-			echo $row['category_name'];
-			echo "<br/>";
-		}
-	?>
-        &nbsp;</td>
-      <td>&nbsp;</td>
-      <td><?php
-    	include ("../scripts/_getAllProductsFormatted.php");
-		while($row = mysql_fetch_array($result)){
-			echo '<a href="viewCalculator.php?ID='.$row['product_id'].'"><img src="../buttons/button_view.jpg" width="71" height="17" /></a>';
-			echo "<br/>";
-		}
-	?>
-        &nbsp;</td>
-    </tr>
   </table>
 </div>
 <div id="apDiv2">
@@ -104,3 +78,5 @@
     </table>
   </form>
 </div>
+</body>
+</html>
